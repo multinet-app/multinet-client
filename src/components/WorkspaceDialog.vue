ab8f2e842f301b8c72c21541668877c777115997
Roni Choudhury <roni.choudhury@kitware.com>
2020-03-16 13:42:30 -0400

    Display error message if something goes wrong
    The new try-catch block wraps the actual API call, then filters the
    error code in the catch handler to install an appropriate error message.

--- src/components/WorkspaceDialog.vue
+++ src/components/WorkspaceDialog.vue
@@ -119,13 +119,21 @@ export default Vue.extend({
   methods: {
     async create() {
       if (this.newWorkspace) {
-        const response = await api.createWorkspace(this.newWorkspace);
+        try {
+          const response = await api.createWorkspace(this.newWorkspace);
 
-        if (response) {
-          this.$router.push(`/workspaces/${this.newWorkspace}`);
-          this.$emit('created', this.newWorkspace);
-          this.newWorkspace = '';
-          this.dialog = false;
+          if (response) {
+            this.$router.push(`/workspaces/${this.newWorkspace}`);
+            this.$emit('created', this.newWorkspace);
+            this.newWorkspace = '';
+            this.dialog = false;
+          }
+        } catch (err) {
+          if (err.status === 409) {
+            this.error = `Workspace "${err.data}" already exists!`;
+          } else {
+            this.error = `Error creating workspace: ${err.status}`;
+          }
         }
       }
     },
