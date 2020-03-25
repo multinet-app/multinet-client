9918516e96f4cced939fcced3b6e426dec52a095
Roni Choudhury <roni.choudhury@kitware.com>
2020-03-25 16:05:42 -0400

    Add error handling clause for unprocessable CSV

--- src/components/FileUploadForm.vue
+++ src/components/FileUploadForm.vue
@@ -167,6 +167,8 @@ export default Vue.extend({
       } catch (err) {
         if (err.status === 409) {
           this.tableCreationError = `Table "${fileName}" already exists`;
+        } else if (err.status === 415) {
+          this.tableCreationError = 'Data could not be read as CSV';
         } else {
           this.tableCreationError = 'Unknown error; please see developer console for details';
           throw err;
