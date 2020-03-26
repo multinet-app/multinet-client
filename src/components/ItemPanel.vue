cfd167bd4444193094b27a9068f6c3d637264455
Jared Tomeck <jared.tomeck@kitware.com>
2020-03-26 14:23:13 -0400

    Update icon colors in lists
    This commit will improve the UI for selecting items in the list by adding more visual difference between the icon and a checked checkbox.

--- src/components/ItemPanel.vue
+++ src/components/ItemPanel.vue
@@ -27,34 +27,37 @@
     <v-divider></v-divider>
 
     <template v-if="items.length > 0">
-      <v-hover
-        v-for="item in items"
-        :key="item"
-      >
-        <v-list-item
-          active-class="grey lighten-4"
-          ripple
-          slot-scope="{ hover }"
-          :to="`/workspaces/${workspace}/${routeType}/${item}`"
+      <v-list-item-group color="primary">
+        <v-hover
+          v-for="item in items"
+          :key="item"
         >
-          <v-list-item-action @click.prevent>
-            <v-icon
-              color="blue lighten-1"
-              v-if="!hover && !checkbox[item]"
-            >{{icon}}</v-icon>
+          <v-list-item
+            active-class="grey lighten-4"
+            ripple
+            slot-scope="{ hover }"
+            :to="`/workspaces/${workspace}/${routeType}/${item}`"
+          >
+            <v-list-item-action @click.prevent>
+              <v-icon
+                class="item-icon"
+                v-if="!hover && !checkbox[item]"
+              >{{icon}}</v-icon>
 
-            <v-checkbox
-              class="ws-detail-checkbox"
-              v-else
-              v-model="checkbox[item]"
-            ></v-checkbox>
-          </v-list-item-action>
+              <v-checkbox
+                class="ws-detail-checkbox"
+                v-else
+                v-model="checkbox[item]"
+              ></v-checkbox>
+            </v-list-item-action>
 
-          <v-list-item-content>
-            <v-list-item-title>{{item}}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-hover>
+            <v-list-item-content>
+              <v-list-item-title>{{item}}</v-list-item-title>
+            </v-list-item-content>
+
+          </v-list-item>
+        </v-hover>
+      </v-list-item-group>
     </template>
     <div
       class="ws-detail-empty-list"
@@ -136,4 +139,9 @@ export default Vue.extend({
   padding: 40px 40px 55px;
   text-align: center;
 }
+
+.item-icon {
+  opacity: .4;
+}
+
 </style>
