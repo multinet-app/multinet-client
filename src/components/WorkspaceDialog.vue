7f646353f38cc119fcc0f2148c53fadfcb552526
Roni Choudhury <roni.choudhury@kitware.com>
2020-04-03 18:52:41 -0400

    Bind the v-slot activator to an empty object
    This avoids all four possible problems:
    - without the activator binding, the DOM behaves badly (the new
      workspace button attaches to the getting started popover)
    - with a binding to `{ on }` the linter complains of a shadowed
      variable
    - with a binding to any name besides `on`, the linter complains about an
      unused variable
    - omitting the binding altogether (but leaving the `v-slot:activator`
      attribute) passes linting but results in a runtime error

--- src/components/WorkspaceDialog.vue
+++ src/components/WorkspaceDialog.vue
@@ -8,7 +8,7 @@
     origin="center center"
     transition="scale-transition"
   >
-    <template v-slot:activator>
+    <template v-slot:activator="{ }">
       <v-dialog
         v-model="dialog"
         class="ws-dialogue"
