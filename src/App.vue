365ed8913361ca69ea656fa84bfdce962b93cfa7
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-09-08 20:21:07 -0400

    Add handling of loginToken url fragment

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,15 @@
 </template>
 
 <script lang="ts">
-import Sidebar from '@/components/Sidebar.vue';
 import Vue from 'vue';
-import store from '@/store';
 
+import store from '@/store';
+import Sidebar from '@/components/Sidebar.vue';
+import api from '@/api';
+import { saveLoginToken } from '@/utils/localStorage';
 import apps from './appregistry.json';
 
+const loginTokenRegex = /^#loginToken=(\S+)$/;
 export default Vue.extend({
   name: 'App',
 
@@ -27,6 +30,33 @@ export default Vue.extend({
 
   created() {
     store.dispatch.fetchUserInfo();
+    this.checkUrlForLogin();
+  },
+  methods: {
+    /**
+     * When login is completed, the server will redirect the browser back here,
+     * with an appended url fragment of `loginToken=<token>`. This function checks
+     * for this fragment, and if it exists, it pops that token from the URL, saves
+     * it in localStorage, and re-fetches app info, using this new token.
+     */
+    checkUrlForLogin() {
+      if (this.$route.hash) {
+        const result = this.$route.hash.match(loginTokenRegex);
+
+        if (result && result.length >= 2) {
+          const loginToken = result[1];
+          api.client.setAuthToken(loginToken);
+
+          const currentRouteName = this.$route.name;
+          const name = currentRouteName || 'home';
+
+          this.$router.replace({ name });
+          saveLoginToken(loginToken);
+          store.dispatch.fetchUserInfo();
+          store.dispatch.fetchWorkspaces();
+        }
+      }
+    },
   },
 });
 </script>
