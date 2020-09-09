b790f72932ff149d913c9dad9b37ca46f10115fb
Jacob Nesbitt <jjnesbitt2@gmail.com>
2020-09-08 20:16:04 -0400

    Add utils for localStorage interface

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,13 @@
+export const localStorageLoginTokenKey = 'loginToken';
+
+export function getLoginToken(): string | null {
+  return localStorage.getItem(localStorageLoginTokenKey);
+}
+
+export function saveLoginToken(token: string) {
+  localStorage.setItem(localStorageLoginTokenKey, token);
+}
+
+export function deleteLoginToken() {
+  localStorage.removeItem(localStorageLoginTokenKey);
+}
