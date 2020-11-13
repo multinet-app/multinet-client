60299ca6845a33ce45e07a791e5864a3c310b61f
Roni Choudhury <roni.choudhury@kitware.com>
2020-11-13 13:32:53 -0500

    Move "create table" dialog content to inside stepper

--- src/components/TableDialog.vue
+++ src/components/TableDialog.vue
@@ -47,14 +47,92 @@
 
       <v-stepper-items>
         <v-stepper-content step="1">
-          <h1>hi</h1>
+          <v-card>
+            <v-card>
+              <v-card-text class="px-4 pt-4 pb-1">
+                <v-layout wrap>
+                  <v-flex>
+                    <v-file-input
+                      :error-messages="fileUploadError"
+                      label="Upload File"
+                      prepend-inner-icon="attach_file"
+                      prepend-icon=""
+                      single-line
+                      clearable
+                      dense
+                      outlined
+                      @change="handleFileInput"
+                    />
+                  </v-flex>
+                </v-layout>
+                <v-layout wrap>
+                  <v-flex>
+                    <v-text-field
+                      v-model="fileName"
+                      :error-messages="tableCreationError"
+                      label="Table Name"
+                      outlined
+                      dense
+                    />
+                  </v-flex>
+                </v-layout>
+                <v-row no-gutters>
+                  <v-col cols="4">
+                    <v-text-field
+                      v-model="keyField"
+                      label="Key Column"
+                      append-icon="restore"
+                      outlined
+                      dense
+                      @click:append="restoreKeyField"
+                    />
+                  </v-col>
+                  <v-col cols="4">
+                    <v-checkbox
+                      v-model="overwrite"
+                      class="mt-1 ml-2"
+                      dense
+                      label="Overwrite Default Key"
+                      outlined
+                    />
+                  </v-col>
+                </v-row>
 
-          <v-btn
-            color="primary"
-            @click="step = 2"
-          >
-            Next
-          </v-btn>
+                <v-row>
+                  <v-col
+                    v-for="(type, field) in columnType"
+                    :key="field"
+                    cols="3"
+                  >
+                    <v-select
+                      v-model="columnType[field]"
+                      outlined
+                      dense
+                      :label="field"
+                      :items="multinetTypes"
+                    />
+                  </v-col>
+                </v-row>
+              </v-card-text>
+
+              <v-divider />
+
+              <v-card-actions class="px-4 py-3">
+                <v-spacer />
+                <v-btn
+                  :disabled="createDisabled"
+                  @click="step = 2"
+                >
+                  Next
+                </v-btn>
+              </v-card-actions>
+
+              <v-progress-linear
+                v-if="uploading"
+                :value="uploadProgress"
+              />
+            </v-card>
+          </v-card>
         </v-stepper-content>
 
         <v-stepper-content step="2">
@@ -93,98 +171,6 @@
         </v-stepper-content>
       </v-stepper-items>
     </v-stepper>
-
-    <v-card>
-      <v-card-title
-        class="headline pb-0 pt-3"
-        primary-title
-      >
-        Create Table
-      </v-card-title>
-      <v-card>
-        <v-card-text class="px-4 pt-4 pb-1">
-          <v-layout wrap>
-            <v-flex>
-              <v-file-input
-                :error-messages="fileUploadError"
-                label="Upload File"
-                prepend-inner-icon="attach_file"
-                prepend-icon=""
-                single-line
-                clearable
-                dense
-                outlined
-                @change="handleFileInput"
-              />
-            </v-flex>
-          </v-layout>
-          <v-layout wrap>
-            <v-flex>
-              <v-text-field
-                v-model="fileName"
-                :error-messages="tableCreationError"
-                label="Table Name"
-                outlined
-                dense
-              />
-            </v-flex>
-          </v-layout>
-          <v-row no-gutters>
-            <v-col cols="4">
-              <v-text-field
-                v-model="keyField"
-                label="Key Column"
-                append-icon="restore"
-                outlined
-                dense
-                @click:append="restoreKeyField"
-              />
-            </v-col>
-            <v-col cols="4">
-              <v-checkbox
-                v-model="overwrite"
-                class="mt-1 ml-2"
-                dense
-                label="Overwrite Default Key"
-                outlined
-              />
-            </v-col>
-          </v-row>
-
-          <v-row>
-            <v-col
-              v-for="(type, field) in columnType"
-              :key="field"
-              cols="3"
-            >
-              <v-select
-                v-model="columnType[field]"
-                outlined
-                dense
-                :label="field"
-                :items="multinetTypes"
-              />
-            </v-col>
-          </v-row>
-        </v-card-text>
-
-        <v-divider />
-
-        <v-card-actions class="px-4 py-3">
-          <v-spacer />
-          <v-btn
-            :disabled="createDisabled"
-            @click="createTable"
-          >
-            Create
-          </v-btn>
-        </v-card-actions>
-        <v-progress-linear
-          v-if="uploading"
-          :value="uploadProgress"
-        />
-      </v-card>
-    </v-card>
   </v-dialog>
 </template>
 
