33ed0f24f4c91f5a9be2d5c9d45e3f4788caf210
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-03-10 17:23:54 -0500

    WIP add multi-table view

--- src/components/NetworkMultiCSVUploadForm.vue
+++ src/components/NetworkMultiCSVUploadForm.vue
@@ -11,20 +11,65 @@
 
     <!-- Data tables -->
     <template v-if="fileSamples.length">
-      <v-data-table
-        v-for="sample in fileSamples"
-        :key="sample.name"
-        style="width: 40%"
-        hide-default-footer
-        :headers="sample.headers"
-        :items="sample.rows"
-      />
+      <v-row
+        v-for="(samples, i) in chunkedFileSamples"
+        :key="`${i*2}-${(i*2)+1}`"
+        no-gutters
+      >
+        <v-col
+          v-for="sample in samples"
+          :key="sample.name"
+          cols="6"
+        >
+          <span>{{ sample.name }}</span>
+          <v-data-table
+            class="upload-preview"
+            hide-default-footer
+            hide-default-header
+            :headers="sample.headers"
+            :items="sample.rows"
+            disable-sort
+          >
+            <template v-slot:header="{ props: { headers } }">
+              <thead dark>
+                <tr>
+                  <th
+                    v-for="header in headers"
+                    :key="header.text"
+                    class="pt-2 pb-4"
+                  >
+                    {{ header.text }}
+                    <v-menu>
+                      <template v-slot:activator="{ on }">
+                        <v-icon
+                          small
+                          v-on="on"
+                        >
+                          link
+                        </v-icon>
+                      </template>
+                      <v-list>
+                        <v-list-item>
+                          Choose source
+                        </v-list-item>
+                      </v-list>
+                    </v-menu>
+                  </th>
+                </tr>
+              </thead>
+            </template>
+          </v-data-table>
+        </v-col>
+      </v-row>
     </template>
   </v-card>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watchEffect } from '@vue/composition-api';
+import {
+  computed, defineComponent, ref, watchEffect,
+} from '@vue/composition-api';
+import { chunk } from 'lodash';
 import Papa from 'papaparse';
 import { DataTableHeader } from 'vuetify';
 
@@ -60,10 +105,20 @@ export default defineComponent({
       fileSamples.value = (await samplesPromise) as CSVPreview[];
     });
 
+    // Chunk samples to allow for row rendering
+    const chunkedFileSamples = computed(() => chunk(fileSamples.value, 2));
+
     return {
       files,
       fileSamples,
+      chunkedFileSamples,
     };
   },
 });
 </script>
+<style scoped>
+.upload-preview table th {
+  background-color: #1976d2 !important;
+  color: #fff !important;
+}
+</style>
