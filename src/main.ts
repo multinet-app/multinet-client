92ab1ec6508ce49dc9598223ac3f04a3762e12ba
Jack Wilburn <jackwilburn@tutanota.com>
2023-03-14 14:42:16 -0600

    Fix issues with invalidating cookies and restoring logins

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import * as Sentry from '@sentry/vue';
 import Vue from 'vue';
-import { writeSharedLoginCookie, invalidateSharedLoginCookie } from 'multinet';
+import { writeSharedLoginCookie, invalidateSharedLoginCookie, readSharedLoginCookie } from 'multinet';
 import App from './App.vue';
 import api from './api';
 import oauthClient from './oauth';
-import { sentryDsn, oauthClientId } from './environment';
+import { sentryDsn, oauthClientId, prodBuild } from './environment';
 import vuetify from './vuetify';
 import router from './router';
 import store from './store';
@@ -12,11 +12,22 @@ import './vuegtag';
 
 Vue.config.productionTip = false;
 
+const loginTokenKey = `oauth-token-${oauthClientId}`;
+const sharedLoginCookie = readSharedLoginCookie();
+if (prodBuild && sharedLoginCookie !== null) {
+  localStorage.setItem(loginTokenKey, sharedLoginCookie);
+}
+
 oauthClient.maybeRestoreLogin().then(() => {
   Object.assign(api.axios.defaults.headers.common, oauthClient.authHeaders);
 
-  const tokenString = localStorage.getItem(`oauth-token-${oauthClientId}`);
-  if (tokenString) {
+  // If logged out, remove the local storage item
+  if (!Object.keys(oauthClient.authHeaders).includes('Authorization')) {
+    localStorage.removeItem(loginTokenKey);
+  }
+
+  const tokenString = localStorage.getItem(loginTokenKey);
+  if (tokenString !== null) {
     writeSharedLoginCookie(tokenString);
   } else {
     invalidateSharedLoginCookie();
