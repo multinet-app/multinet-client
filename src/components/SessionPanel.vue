e7c8306ce863c9ae2e483a441c05bd8ab8bdeaa6
Jack Wilburn <jackwilburn@tutanota.com>
2023-07-06 15:33:47 -0600

    Update styling given recent feedback

--- src/components/SessionPanel.vue
+++ src/components/SessionPanel.vue
@@ -8,57 +8,62 @@
 
     <v-divider />
 
-    <v-carousel v-if="sessionPages.length > 0 || loading" height="90" class="mt-4" continuous hide-delimiters>
-      <v-carousel-item v-for="sessionPage, pageIndex in sessionPages" :key="`page${pageIndex}`">
-        <v-row class="px-16 mx-16">
-          <v-col v-for="session, idx in sessionPage" :key="`session-${session.visapp}-${idx}-${session.id}`" :cols="12 / sessionsPerPage">
-            <v-skeleton-loader v-if="session.id === '_skeleton'" type="card" height="90" />
-            <v-card v-else color="grey lighten-3" height="90" hover @click="openSession(session)">
-              <v-card-text class="pt-3 dont-wrap">
-                <v-tooltip right open-delay="500">
-                  <template #activator="{ on, attrs }">
-                    <p
-                      v-bind="attrs"
-                      class="mb-0 dont-wrap"
-                      v-on="on"
-                    >
-                      <strong class="black--text">Name:</strong> {{ session.name }}
-                    </p>
-                  </template>
-                  <span>{{ session.name }}</span>
-                </v-tooltip>
-                <strong class="black--text">App:</strong> {{ session.visapp }} <br />
-                <v-tooltip right open-delay="500">
-                  <template #activator="{ on, attrs }">
-                    <p
-                      v-bind="attrs"
-                      class="mb-0 dont-wrap"
-                      v-on="on"
-                    >
-                      <strong class="black--text">{{ session.network !== undefined ? 'Network' : 'Table' }}:</strong> {{ networks.find((network) => network.id === session.network)?.name || tables.find((table) => table.id === session.table)?.name }}
-                    </p>
-                  </template>
-                  <span>{{ networks.find((network) => network.id === session.network)?.name || tables.find((table) => table.id === session.table)?.name }}</span>
-                </v-tooltip>
+    <v-row v-if="sessions.length > 0" class="px-16 mx-16 mt-2" :style="{ height: '160px', maxHeight: '160px', overflow: 'scroll' }">
+      <v-col v-for="session, idx in sessions" :key="`session-${session.visapp}-${idx}-${session.id}`" cols="4">
+        <v-skeleton-loader v-if="session.id === '_skeleton'" type="card" height="120" />
+        <v-card v-else color="grey lighten-3" height="120" hover @click="openSession(session)">
+          <v-card-text class="pa-6 pt-7 dont-wrap">
+            <v-tooltip right open-delay="500">
+              <template #activator="{ on, attrs }">
+                <p
+                  v-bind="attrs"
+                  class="mb-0 dont-wrap"
+                  v-on="on"
+                >
+                  <strong class="black--text">Name:</strong> {{ session.name }}
+                </p>
+              </template>
+              <span>{{ session.name }}</span>
+            </v-tooltip>
+            <strong class="black--text">App:</strong> {{ session.visapp }} <br />
+            <v-tooltip right open-delay="500">
+              <template #activator="{ on, attrs }">
+                <p
+                  v-bind="attrs"
+                  class="mb-0 dont-wrap"
+                  v-on="on"
+                >
+                  <strong class="black--text">{{ session.network !== undefined ? 'Network' : 'Table' }}:</strong> {{ networks.find((network) => network.id === session.network)?.name || tables.find((table) => table.id === session.table)?.name }}
+                </p>
+              </template>
+              <span>{{ networks.find((network) => network.id === session.network)?.name || tables.find((table) => table.id === session.table)?.name }}</span>
+            </v-tooltip>
+            <v-tooltip top color="error">
+              <template #activator="{ on, attrs }">
                 <v-btn
                   fab
-                  color="red"
+                  tile
+                  color="grey lighten-3"
                   top
                   right
                   absolute
                   x-small
-                  :style="{ right: '-16px' }"
+                  elevation="0"
+                  :style="{ top: '0px', right: '0px' }"
+                  v-bind="attrs"
+                  v-on="on"
                   @click="deleteClicked(session)"
                   @click.stop
                 >
                   <v-icon>mdi-close</v-icon>
                 </v-btn>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-carousel-item>
-    </v-carousel>
+              </template>
+              <span>Delete Session</span>
+            </v-tooltip>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
 
     <div v-else class="my-4 text-center">
       <v-icon color="blue lighten-1">
@@ -81,22 +86,15 @@ const props = defineProps<{
   loading: boolean;
 }>();
 
-const sessions = computed(() => store.getters.sessions);
+const sessions = computed(() => {
+  const storeSessions = store.getters.sessions;
+  if (storeSessions.length > 0) {
+    return storeSessions;
+  }
+  return Array(3).fill({ id: '_skeleton' });
+});
 const networks = computed(() => store.getters.networks);
 const tables = computed(() => store.getters.tables);
-
-const sessionsPerPage = 3;
-const sessionPages = computed(() => {
-  const pages = [];
-  if (props.loading) {
-    pages.push(Array(sessionsPerPage).fill({ id: '_skeleton' }));
-  } else {
-    for (let i = 0; i < sessions.value.length; i += sessionsPerPage) {
-      pages.push(sessions.value.slice(i, i + sessionsPerPage));
-    }
-  }
-  return pages;
-});
 
 function openSession(session: Session) {
   const type = session.network !== undefined ? 'network' : 'table';
