b25a70a758426d70f10ef139fdeebfe503a5f99f
Jack Wilburn <jackwilburn@tutanota.com>
2023-07-12 13:41:57 -0600

    Add dialog to rename sessions

--- src/components/SessionPanel.vue
+++ src/components/SessionPanel.vue
@@ -11,7 +11,14 @@
     <v-row v-if="sessions.length > 0" class="px-16 mx-16 mt-2" :style="{ height: '160px', maxHeight: '160px', overflow: 'scroll' }">
       <v-col v-for="session, idx in sessions" :key="`session-${session.visapp}-${idx}-${session.id}`" cols="4">
         <v-skeleton-loader v-if="session.id === '_skeleton'" type="card" height="120" />
-        <v-card v-else color="grey lighten-3" height="120" hover @click="openSession(session)">
+        <v-card
+          v-else
+          color="grey lighten-3"
+          height="120"
+          hover
+          @click="openSession(session)"
+          @contextmenu="showContextMenu($event, session)"
+        >
           <v-card-text class="pa-6 pt-7 dont-wrap">
             <v-tooltip right open-delay="500">
               <template #activator="{ on, attrs }">
@@ -70,6 +77,45 @@
         mdi-information
       </v-icon> Click on the <strong>Visualize</strong> button in the <strong>Network</strong> or <strong>Table</strong> panel to create a new session.
     </div>
+
+    <v-menu
+      v-model="contextMenu.show"
+      :position-x="contextMenu.left"
+      :position-y="contextMenu.top"
+    >
+      <v-list>
+        <v-list-item @click="openRenamingDialog">
+          Rename Session
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    <v-dialog v-model="showRenamingDialog">
+      <v-card>
+        <v-card-title>
+          Rename Session
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="newSessionName"
+            label="New Session Name"
+            outlined
+            dense
+            clearable
+            :rules="[v => !!v || 'Name is required']"
+          />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" text @click="showRenamingDialog = false">
+            Cancel
+          </v-btn>
+          <v-btn color="primary" text @click="renameSession">
+            Rename
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -77,8 +123,9 @@
 import store from '@/store';
 import type { App } from '@/types';
 import type { Session } from 'multinet';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { deleteSession, visualizeWithExistingSession } from '@/utils/sessionHelpers';
+import api from '@/api';
 
 const props = defineProps<{
   apps: { network_visualizations: App[]; table_visualizations: App[] };
@@ -112,6 +159,36 @@ async function deleteClicked(session: Session) {
   const type = session.network !== undefined ? 'network' : 'table';
   store.commit.deleteSessionFromStore(session.id);
   await deleteSession(session.id, type);
+}
+
+const contextMenu = ref({
+  show: false, top: 0, left: 0, session: null as Session | null,
+});
+
+function showContextMenu(event: MouseEvent, session: Session) {
+  contextMenu.value = {
+    show: true,
+    top: event?.clientY,
+    left: event?.clientX,
+    session,
+  };
+  event.preventDefault();
+}
+
+const showRenamingDialog = ref(false);
+const newSessionName = ref('');
+function openRenamingDialog() {
+  showRenamingDialog.value = true;
+  if (contextMenu.value.session !== null) {
+    newSessionName.value = contextMenu.value.session.name;
+  }
+}
+function renameSession() {
+  if (contextMenu.value.session !== null) {
+    contextMenu.value.session.name = newSessionName.value;
+    api.updateSession();
+  }
+  showRenamingDialog.value = false;
 }
 </script>
 
