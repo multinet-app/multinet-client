b2cc2e16093f854f7b6653fbb043aa32f447e71f
Jack Wilburn <jackwilburn@tutanota.com>
2023-08-15 13:48:18 -0600

    Add a try-catch onto visualize ..., so that the vis app always opens

--- src/utils/sessionHelpers.ts
+++ src/utils/sessionHelpers.ts
@@ -4,9 +4,13 @@ import type { App } from '@/types';
 import type { Network, Table } from 'multinet';
 
 export async function visualizeWithNewSession(item: Network | Table, app: App, type: 'network' | 'table', workspace: string) {
-  const session = await api.createSession(workspace, item.id, type, app.name, new Date().toISOString());
-  window.open(`${app.url}/?workspace=${workspace}&${type}=${item.name}&sessionId=${session.id}`, '_blank');
-  await store.dispatch.fetchWorkspace(workspace);
+  try {
+    const session = await api.createSession(workspace, item.id, type, app.name, new Date().toISOString());
+    window.open(`${app.url}/?workspace=${workspace}&${type}=${item.name}&sessionId=${session.id}`, '_blank');
+    await store.dispatch.fetchWorkspace(workspace);
+  } catch (error) {
+    window.open(`${app.url}/?workspace=${workspace}&${type}=${item.name}`, '_blank');
+  }
 }
 
 export async function visualizeWithExistingSession(item: Network | Table, app: App, type: 'network' | 'table', workspace: string, sessionId: number) {
